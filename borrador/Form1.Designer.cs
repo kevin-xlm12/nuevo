ca34b753639e26684b624e5b8bc198c1f85335a3
kevin-xlm12 <aler8860gmail.com>
2023-04-28 15:30:29 -0300

    nuevo commit

---
+++ borrador/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace borrador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cboDias = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDias = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(42, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(367, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(120, 160);
+            this.listBox1.TabIndex = 1;
+            // 
+            // cboDias
+            // 
+            this.cboDias.FormattingEnabled = true;
+            this.cboDias.Location = new System.Drawing.Point(210, 13);
+            this.cboDias.Name = "cboDias";
+            this.cboDias.Size = new System.Drawing.Size(121, 21);
+            this.cboDias.TabIndex = 2;
+            this.cboDias.SelectedIndexChanged += new System.EventHandler(this.cboDias_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblDias
+            // 
+            this.lblDias.AutoSize = true;
+            this.lblDias.Location = new System.Drawing.Point(109, 29);
+            this.lblDias.Name = "lblDias";
+            this.lblDias.Size = new System.Drawing.Size(35, 13);
+            this.lblDias.TabIndex = 4;
+            this.lblDias.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(298, 226);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "iii"});
+            this.comboBox1.Location = new System.Drawing.Point(194, 300);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 462);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblDias);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cboDias);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox cboDias;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblDias;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ borrador/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace borrador
+{
+    public partial class Form1 : Form
+    {
+        enum dias { lunes, martes, miercoles, jueves, viernes, sabado, domingo };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int []nums = new int[6]{4,3,32,21,2,3,};
+        String []nombres = { "juan", "pedro", "javier", "roberto", "pedro", "raul" };
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          // foreach (int i in nums)
+          //  {
+          //     MessageBox.Show(i.ToString());
+          // }
+          
+            listBox1.Items.AddRange(nombres);
+            dataGridView1.Columns.Add("nombres","");
+            foreach (String i in nombres)
+            {
+                dataGridView1.Rows.Add(i);
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cboDias.Items.AddRange(Enum.GetNames(typeof(dias)));
+            cboDias.SelectedIndex = 0;
+           
+
+        }
+
+        private void cboDias_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dias miDia;
+            miDia = (dias)cboDias.SelectedIndex;
+             switch (miDia)
+            {
+              case dias.domingo:
+              lblDias.Text = "feriado  " + Enum.GetName(typeof(dias), miDia);
+              break;
+             default:
+             lblDias.Text ="laborable";
+              break;
+           }
+        }
+    }
+}
